d3004944ccbc13de3b9312998ecc989209522525
Will Schabel <willschabel@ksu.edu>
2019-09-20 14:28:56 -0500

    Working.

---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// Interace for items that can not be blended.
+    /// </summary>
+    interface IBlendable
+    {
+        /// <summary>
+        /// Blends the object
+        /// </summary>
+        /// <returns>A string describing the blended object.</returns>
+        string Blend();
+    }
+}
--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -6,7 +7,26 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<IBlendable> blendables = new List<IBlendable>();
+            blendables.Add(new Strawberry());
+            blendables.Add(new Banana());
+            blendables.Add(new CellPhone());
+            blendables.Add(new IceCubes());
+            for(int i = 0; i < 10; i++)
+            {
+                blendables.Add(new Mango());
+            }
+
+
+            Banana b = new Banana();
+            Console.WriteLine(b.Blend());
+            string mess = "";
+            blendables.Add(b);
+            foreach(IBlendable blendable in blendables)
+            {
+                mess += blendable.Blend();
+            }
+            Console.WriteLine(mess);
         }
     }
 }
